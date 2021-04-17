5c9592a91b1943f4a02c150140d2926f7a6d9654
Sergio Geronimo <mc.ryunosuke@gmail.com>
2021-04-16 21:28:14 -0500

    Archivos basicos creados

---
+++ app/src/main/java/com/example/appti/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.appti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appti.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
